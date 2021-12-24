875635857f02ce11ef1bafd8191e543618c45911
Andres Fonseca <andresmfonseca@outlook.com>
2021-12-24 00:50:57 -0500

    Configuracion de las preguntas, puntos y nivel

--- src/pages/Jugar.js
+++ src/pages/Jugar.js
@@ -2,18 +2,25 @@ import { Fragment, useState, useEffect } from "react";
 import axios from "axios";
 import { Modal } from "react-bootstrap";
 import config from "../config/config.json";
+import { Spinner } from "react-bootstrap";
 
 function Jugar() {
 
+    const [showLoading, setShowLoading] = useState(true);
     const [preguntas, setPreguntas] = useState([]);
+    const [respuestas, setRespuestas] = useState([]);
     const [puntos, setPuntos] = useState(0);
+    const [puntosPregunta, setPuntosPregunta] = useState(0);
     const [nivel, setNivel] = useState(1);
+    const [radioButton, setRadioButton] = useState(0);
     const [jugador, setJugador] = useState({
         nombre: "",
         apellido: "",
         puntos: 0,
         nivel: 1
     });
+    // Mostrar las respuestas en forma aleatoria
+
 
     const [modal, setModal] = useState({
         mostrar: false,
@@ -22,6 +29,7 @@ function Jugar() {
 
     useEffect(() => {
         if (jugador.nombre === "") {
+            setShowLoading(true);
             setModal({
                 mostrar: true,
                 titulo: "Nuevo jugador"
@@ -32,23 +40,36 @@ function Jugar() {
     // Salir del juego
     const salir = async () => {
         if (window.confirm("¿Estás seguro de que quieres salir del juego?")) {
-
-            await axios.post(`${config.HOST}/jugador/new`, jugador)
-                .then(res => {
-                    console.log(res);
-                    window.location.href = "/";
-                })
-                .catch(err => {
-                    if (err.response) {
-                        alert(err.response.data.message);
-                    } else {
-                        alert("Error, contacte con el administrador");
-                    }
-                    console.log(err);
-                });
-           
+            handleGuardar();
         }
     };
+
+    // Guardar datos del jugador
+    const handleGuardar = async () => {
+        setJugador({
+            ...jugador,
+            puntos: puntos,
+            nivel: nivel
+        });
+        setPuntos(0);
+        setNivel(1);
+        setPreguntas([]);
+
+        await axios.post(`${config.HOST}/jugador/new`, jugador)
+            .then(res => {
+                console.log(res);
+                window.location.href = "/";
+            })
+            .catch(err => {
+                if (err.response) {
+                    alert(err.response.data.message);
+                } else {
+                    alert("Error, contacte con el administrador");
+                }
+                console.log(err);
+            });
+    };
+
 
     const onInputChange = (e) => {
         const [name, value] = [e.target.name, e.target.value];
@@ -58,8 +79,16 @@ function Jugar() {
         });
     };
 
-    const onSubmit = (e) => {
+    // onRadioChange
+    const onRadioChange = (e) => {
+        console.log(e.target.value);
+        setRadioButton(e.target.value);
+    };
+
+    // Se cierra el modal y queda en el jugador nuevo
+    const guardar = (e) => {
         e.preventDefault();
+        setShowLoading(false);
         setModal({
             mostrar: false,
             titulo: ""
@@ -73,6 +102,60 @@ function Jugar() {
         setModal(paramNuevos);
         window.location.href = "/";
     };
+
+    // Obtener el valor del radio button seleccionado
+
+
+    // Al enviar la respuesta
+    const enviarRespuesta = async (e) => {
+        e.preventDefault();
+
+        const respuesta = radioButton;
+        
+        if (respuesta === preguntas.respuestaCorrecta) {
+            setPuntos(puntos + puntosPregunta);
+            setNivel(nivel + 1);
+        
+
+        } else {
+            alert("Respuesta incorrecta");
+            handleGuardar();
+
+        }
+    };
+
+    // Se obtiene una pregunta segun el nivel
+    useEffect(() => {
+        const obtenerPregunta = async () => {
+            await axios.get(`${config.HOST}/pregunta/nivel/${nivel}`)
+                .then(res => {
+                    setPuntosPregunta(puntos*nivel+10);
+                    console.log(res);
+                    setPreguntas(res.data.pregunta);
+                    const respuestasObtenidas = [
+                        res.data.pregunta.respuesta1,
+                        res.data.pregunta.respuesta2,
+                        res.data.pregunta.respuesta3,
+                        res.data.pregunta.respuestaCorrecta
+                    ];
+
+                    const respuestasAleatorias = respuestasObtenidas.sort(() => Math.random() - 0.5);
+
+                    setRespuestas(respuestasAleatorias);
+                    setShowLoading(false);
+                })
+                .catch(err => {
+                    if (err.response) {
+                        alert(err.response.data.message);
+                    } else {
+                        alert("Error, contacte con el administrador");
+                    }
+                    console.log(err);
+                });
+        };
+        obtenerPregunta();
+    }, [nivel, puntos]);
+
 
     return (
         <Fragment>
@@ -111,50 +194,52 @@ function Jugar() {
                 A continuación se muestraran una serie de preguntas que iran cambiando de difuculta.
             </div>
             <div className="container mt-4 shadow p-3 mb-5 bg-body rounded">
-                Puntos por esta pregunta: {puntos * nivel + 10}
+                Puntos por esta pregunta: {puntosPregunta}
                 <div className="container mt-4 shadow p-3 mb-5 bg-body rounded">
-                    <form>
-                        <div className="row">
-                            <div className="col-md-4">
+                    {showLoading ? <div className="col-sm-12 text-center"><Spinner animation="border" variant="primary" /></div> :
+                        <form onSubmit={enviarRespuesta}>
+                            <div className="row">
+                                <div className="col-md-4">
 
-                                <div className="col-md-4 d-flex align-items-center">
-                                    <h5>Pregunta 1 </h5>
+                                    <div className="col-md-4 d-flex align-items-center">
+                                        <h5>{preguntas.pregunta} </h5>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="form-check">
-                                    <input className="form-check-input" type="radio" name="exampleRadios" id="respuesta1" value="option1" />
-                                    <label className="form-check-label" htmlFor="repuesta1">
-                                        Respuesta 1
-                                    </label>
+                                <div className="col-md-6">
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" name="exampleRadios" id="respuesta1" value={respuestas[0]} onChange={onRadioChange} />
+                                        <label className="form-check-label" htmlFor="repuesta1">
+                                            {respuestas[0]}
+                                        </label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" name="exampleRadios" id="respuesta2" value={respuestas[1]} onChange={onRadioChange} />
+                                        <label className="form-check-label" htmlFor="respuesta2">
+                                            {respuestas[1]}
+                                        </label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" name="exampleRadios" id="respuesta4" value={respuestas[2]} onChange={onRadioChange} />
+                                        <label className="form-check-label" htmlFor="respuesta3">
+                                            {respuestas[2]}
+                                        </label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" name="exampleRadios" id="respuesta4" value={respuestas[3]} onChange={onRadioChange} />
+                                        <label className="form-check-label" htmlFor="respuesta4">
+                                            {respuestas[3]}
+                                        </label>
+                                    </div>
                                 </div>
-                                <div className="form-check">
-                                    <input className="form-check-input" type="radio" name="exampleRadios" id="respuesta2" value="option1" />
-                                    <label className="form-check-label" htmlFor="respuesta2">
-                                        Respuesta 2
-                                    </label>
-                                </div>
-                                <div className="form-check">
-                                    <input className="form-check-input" type="radio" name="exampleRadios" id="respuesta4" value="option1" />
-                                    <label className="form-check-label" htmlFor="respuesta3">
-                                        Respuesta 3
-                                    </label>
-                                </div>
-                                <div className="form-check">
-                                    <input className="form-check-input" type="radio" name="exampleRadios" id="respuesta4" value="option1" />
-                                    <label className="form-check-label" htmlFor="respuesta4">
-                                        Respuesta 4
-                                    </label>
-                                </div>
-                            </div>
-                            <div className="col-md-2">
-                                <button type="button" className="btn btn-primary btn-lg btn-block mt-4">
-                                    Enviar
-                                </button>
+                                <div className="col-md-2">
+                                    <button type="submit" className="btn btn-primary btn-lg btn-block mt-4">
+                                        Enviar
+                                    </button>
 
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    }
                 </div>
             </div>
 
@@ -164,7 +249,7 @@ function Jugar() {
                     <Modal.Title>{modal.titulo}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <form onSubmit={onSubmit} >
+                    <form onSubmit={guardar} >
                         <div className="form-group">
                             <label htmlFor="nombre">Nombre</label>
                             <input type="text" className="form-control" id="nombre" placeholder="Nombre" name="nombre" onChange={onInputChange} />
